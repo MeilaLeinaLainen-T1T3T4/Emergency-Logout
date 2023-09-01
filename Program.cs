f651e1b81aae6e6b9ddecf1c377444f3ed5ef7f6
sieve <sieve@DESKTOP-L6FMOFT>
2023-09-02 00:10:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+    static void Main()
+    {
+        LogOutAllUsers();
+        System.Threading.Thread.Sleep(3000);
+        ForceShutdown();
+    }
+
+    static void LogOutAllUsers()
+    {
+        Process.Start("shutdown", "/l /f");
+    }
+
+    static void ForceShutdown()
+    {
+        ExitWindowsEx(0x00000008 | 0x00000004, 0);
+    }
+}
