dc1059abc3bcd7ad266799fba8c9d6cc8a2a9f54
T1|T3|T4 <106811700+T1orT3orT4@users.noreply.github.com>
2023-09-02 00:30:58 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Timers;
 
 class Program
 {
+    static System.Timers.Timer timer;
+
     [DllImport("user32.dll")]
     public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
 
     static void Main()
     {
+        timer = new System.Timers.Timer(5000);
+        timer.Elapsed += TimerElapsed;
+        timer.Start();
+
+        Console.WriteLine("Emergency Logout\n Logging everyone out!");
+        Console.ReadLine();
+    }
+
+    private static void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        Console.WriteLine("Timer elapsed at: " + DateTime.Now);
         LogOutAllUsers();
         System.Threading.Thread.Sleep(3000);
         ForceShutdown();
